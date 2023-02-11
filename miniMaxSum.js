88729d3c6093366a538b56b29041fe15f0d23136
GilTorch <infotorch2014@gmail.com>
2023-02-11 00:36:27 -0500

    Solved the algorith miniMaxSum

---
+++ miniMaxSum.js
@@ -0,0 +1,31 @@
+// Timer: I took around 15 mins (Too Long)
+
+function miniMaxSum(arr) {
+    // Write your code here
+    let min = Math.min(...arr)
+    let max = Math.max(...arr)
+
+    let arrCopy1 = [...arr]
+    let arrCopy2 = [...arr]
+
+    let maxSum = 0; 
+    let minSum = 0;
+
+    let minIndex = arr.findIndex(el => el === min)
+    let maxIndex = arr.findIndex(el => el === max)
+
+    arrCopy1.splice(minIndex,1)
+
+    arrCopy2.splice(maxIndex,1)
+
+
+    for(let i = 0; i < 4; i++){
+        maxSum += arrCopy1[i]
+        minSum += arrCopy2[i]
+    }
+
+    console.log(`${minSum} ${maxSum}`)
+}
+
+console.log(minMaxSum([1, 3, 5, 7, 9]))
+console.log(minMaxSum([1, 2, 3, 4, 5]))
