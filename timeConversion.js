68dc73a83cccda6aaf2c4b11b9029ba2c1dc1886
GilTorch <infotorch2014@gmail.com>
2023-02-15 17:45:54 -0500

    Solved the function timeConversion

---
+++ timeConversion.js
@@ -0,0 +1,32 @@
+    
+    
+    function timeConversion(s) {
+        // Write your code here
+        
+
+        const hourMinutesAndSecondsArray = s.split(":")
+        const isAM = hourMinutesAndSecondsArray[2].includes("AM")
+        
+        let convertedHour = hourMinutesAndSecondsArray[0]
+        let minutes = hourMinutesAndSecondsArray[1]
+        let seconds = hourMinutesAndSecondsArray[2].replace(/AM|PM/,"")
+
+        if(convertedHour === '12' && isAM){
+            console.log('here')
+            convertedHour = '00'
+        }
+
+        let hourAsNumber = Number(convertedHour)
+
+        if(hourAsNumber > 0 && hourAsNumber < 12 && !isAM){
+            convertedHour = hourAsNumber + 12
+        }
+        
+        
+        return `${convertedHour}:${minutes}:${seconds}`
+
+    }
+
+    console.log(timeConversion("07:05:45PM"))
+    console.log(timeConversion("12:01:00:PM"))
+    console.log(timeConversion("12:01:00AM"))
